e2897e5196c32bf256b384e9db108a0133845787
Steve Gledhill <stevegleds@gmail.com>
2019-11-05 17:19:37 +0000

    after call with Lukáš

--- parse64.py
+++ parse64.py
@@ -1,21 +1,19 @@
 import base64
-from zipfile import ZipFile
+from gzip import GzipFile
 
-s = r"H4sIAAAAAAAAAMWTPWvDMBCG\\/0q42QVZ\\/pB8cwrdOrh0CRkU64IFsuzaSkoI+e+V21BMMLRgQsZ7Jd3p4ZHOMFTK9TQcrB8AN2fYDYPRgKBTzCWmGmOGOkeSEMF16a2m1avVq7I2XUgr1amdscYbCi1g81yW2xDve\\/o4kKtOgDwVPAJLR7KATzmLwJuGBq+aDlAUhchiLgqWh17kPPXjUQbIpnX8U9fKObKfRvv6O2g76pVvwxZDTtuTUw2FS4T5R3Jh\\/LW8RPNkFUe1jCydkmWzZLJ4ANnorFpAlvHR0y+ZmHcW35JlXPwJFy+DYxTIcL9MG59qk\\/PasvgxD3KZNj7RJue1yfE73mhL7qQtYSgF6gKTBFOJbKLtpfVk14d34\\/4pLRETtGIeTWZ3lLa9fAFwsc3+MQUAAA==\""
+secret = "H4sIAAAAAAAEACtJLS4BAAx+f9gEAAAA"  # compressed base64 encoded string from 'test' string 
 # s = s.replace("\\", "")
 
-print(s)
-secret_revealed = base64.b64decode(s) 
-print(f"{secret_revealed}")
-
-
-print(f"Hello World 2")
-# print(f"{secret_revealed}")
+print(secret)
+compressed_secret = base64.b64decode(secret)  # compressed bases64 decoded
+print(f"{compressed_secret}")
+with open("newzip.gz", 'wb') as newzip: # i can uncompress using 7 zip successfully after this runs
+    newzip.write(compressed_secret)
 
 def extract_zip(input_zip):
-    input_zip=ZipFile(input_zip)
-    return {name: input_zip.read(name) for name in input_zip.namelist()}
+    return GzipFile(input_zip).open() # this fails 
 
-with open("64.txt", 'r') as file64:
-    unzip = extract_zip(file64)
-print(unzip)
+with open("newzip.gz", 'rb') as file64:
+    zip = extract_zip(file64)
+    print("zip file contains:", zip)
+    
