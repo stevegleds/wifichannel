2198050e672a97d103c96d218166a1a8fb859ee2
stevegleds <stevegleds@gmail.com>
2019-11-05 18:09:19 +0000

    fixed base 64 decoding

--- parse64.py
+++ parse64.py
@@ -1,19 +1,29 @@
 import base64
-from gzip import GzipFile
+import gzip
+""" Takes compressed base64-encoded string and converts to uncompressed decoded string (the original text)
+Step 1 : use base64 to change compressed encoded string to compressed decoded string
+Step 2 : Create local zipped file (gz) containing compressed decoded string. 
+This can be unzipped to read contents
+Step 3 : Open zipped file to extract contents in memory to be used as needed
+
+Returns:
+    [file] -- [zipped file containing decoded text]
+    [string] -- [contains clear text]
+"""
+zipfile = "zipfile.gz"
+
+def extract_zip(input_zip):
+    with gzip.open(input_zip) as f:
+        data = f.read()
+    return data
+
 
 secret = "H4sIAAAAAAAEACtJLS4BAAx+f9gEAAAA"  # compressed base64 encoded string from 'test' string 
 # s = s.replace("\\", "")
-
-print(secret)
 compressed_secret = base64.b64decode(secret)  # compressed bases64 decoded
-print(f"{compressed_secret}")
-with open("newzip.gz", 'wb') as newzip: # i can uncompress using 7 zip successfully after this runs
+with open(zipfile, 'wb') as newzip: # i can uncompress using 7 zip successfully after this runs
     newzip.write(compressed_secret)
 
-def extract_zip(input_zip):
-    return GzipFile(input_zip).open() # this fails 
+extracted_data = extract_zip(zipfile)
+print(extracted_data)
 
-with open("newzip.gz", 'rb') as file64:
-    zip = extract_zip(file64)
-    print("zip file contains:", zip)
-    
