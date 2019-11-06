88b3f1503388202d94be249e33b9aa11aa396910
Steve Gledhill <stevegleds@gmail.com>
2019-11-06 10:28:26 +0000

    Parse64 now works Added filter to keep only wifiscan results

--- findjson.py
+++ findjson.py
@@ -1,5 +1,6 @@
 logfile = "sample.log"
 jsonfile = "sample.json"
+findtext = "<CMD>WIFISCAN"
 result = []
 def clean(entry):
     entry = entry.replace('\\', '')
@@ -9,11 +10,12 @@ def clean(entry):
 with open(logfile) as log:
     with open(jsonfile, 'w') as json:
         for entry in log:
-            print(entry)
-            # clean(entry)
-            entry = entry.replace('\\', '')
-            start = entry.find("{")
-            json_entry = entry[start:]
-            print(f"{json_entry}")
-            json.write(json_entry)
+            if findtext in entry:
+                print(entry)
+                # clean(entry)
+                entry = entry.replace('\\', '')
+                start = entry.find("{")
+                json_entry = entry[start:]
+                print(f"{json_entry}")
+                json.write(json_entry)
             
--- parse64.py
+++ parse64.py
@@ -18,9 +18,13 @@ def extract_zip(input_zip):
     return data
 
 
-secret = "H4sIAAAAAAAEACtJLS4BAAx+f9gEAAAA"  # compressed base64 encoded string from 'test' string 
+secret_test = "H4sIAAAAAAAEACtJLS4BAAx+f9gEAAAA"  # compressed base64 encoded string from 'test' string 
+secret_raw = "H4sIAAAAAAAAAE2OPQ+CMBCG\\/8vNmJSKNd5owuZgwuDQMJxwhCalYFswxPjfrR+D4\\/Pe+16eB4SGnOcw2xgA9QOuIZgWEEhgt8Umx0IidSglZPA7+aUfB07c0ERXY000nMagL+W51mVV1fp4KutU6DzfZnbNCiiLXGZgeWELuFEqg2gGDpGGCTCXQhVif5BSqV36yy6yf48FoPjn\\/Ms9Ocf2btrYf4JxYk9xTBXDrrWro+SHkAwWdkngh8\\/6+QL2SvRC8QAAAA=="
+secret_clean = secret_raw.replace("\\", "")
+print(f"{secret_clean}")
+
 # s = s.replace("\\", "")
-compressed_secret = base64.b64decode(secret)  # compressed bases64 decoded
+compressed_secret = base64.b64decode(secret_clean)  # compressed bases64 decoded
 with open(zipfile, 'wb') as newzip: # i can uncompress using 7 zip successfully after this runs
     newzip.write(compressed_secret)
 
